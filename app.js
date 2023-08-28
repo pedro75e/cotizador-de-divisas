cdc1455cf55269292a9f864b1ebbb9b83a8bad42
Pedro Eduardo Vazquez <pedroeduardo075@gmail.com>
2023-08-28 09:22:47 -0300

    Limpio el código y documento los pasos

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const url = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales'
+const url = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales' //Llamamos a la API
 
 
 fetch(url)
@@ -10,38 +10,38 @@ fetch(url)
             let precioDolar = document.getElementById('dolar')
             precioDolar.innerHTML = el.casa.venta
         } if(el.casa.nombre === "Dolar Blue"){
-            document.getElementById('dolar-venta').innerHTML = "$ " +  el.casa.venta
+            document.getElementById('dolar-venta').innerHTML = "$ " +  el.casa.venta //Mostramos en pantalla el valor de venta del dólar blue
         } if(el.casa.nombre === "Dolar Blue"){
-            document.getElementById('dolar-compra').innerHTML = "$ " + el.casa.compra
+            document.getElementById('dolar-compra').innerHTML = "$ " + el.casa.compra //Mostramos en pantalla el valor de compra del dólar blue
         }
         })
-   /* console.log(data)*/
+
     
 })
 .catch(err => console.log(err))
 
-
+//Hacemos la funcion mostrar para realizar las operaciones necesarias
 function mostrar(valor){
-        fetch('https://dolarsi.com/api/api.php?type=valoresprincipales')
+        fetch('https://dolarsi.com/api/api.php?type=valoresprincipales') //Llamamos nuevamente a la API
         .then(res => res.json())
         .then(data => {
             data.forEach(el => {
                 if(el.casa.nombre === "Dolar Oficial"){
                     let precioDolar = el.casa.venta
 
-                    let valorImp = parseFloat(precioDolar) * valor
+                    let valorImp = parseFloat(precioDolar) * valor //Multiplicamos el monto ingresado más el valor del dólar oficial
 
-                    let impDolar = (valorImp * 30)/100
+                    let impDolar = (valorImp * 30)/100 //Obtenemos el 30% del impuesto solicitado
 
-                    document.getElementById("impuesto").innerHTML = impDolar.toFixed(2)
+                    document.getElementById("impuesto").innerHTML = impDolar.toFixed(2) //Mostramos el valor del dólar con el impuesto al 30%
 
-                    let impuestoDolar = (valorImp * 45)/100
+                    let impuestoDolar = (valorImp * 45)/100 //Obtenemos el 45% de las retenciones solicitadas
 
-                    document.getElementById("retencion").innerHTML = impuestoDolar.toFixed(2)
+                    document.getElementById("retencion").innerHTML = impuestoDolar.toFixed(2) //Mostramos el el valor del dólar con la retención del 45%
 
-                    document.querySelector("#calcular").addEventListener("click", () =>{
-                    let total = valorImp + impDolar + impuestoDolar
-                    document.getElementById("total").innerHTML = total.toFixed(2)
+                    document.querySelector("#calcular").addEventListener("click", () =>{ //Creamos el evento click para el botón calcular
+                    let total = valorImp + impDolar + impuestoDolar //Obtenemos el total del valor del dólar con el impuesto del 30% más la retención del 45%
+                    document.getElementById("total").innerHTML = total.toFixed(2) //Mostramos en pantalla el total del monto solicitado para la compra
                     })
                }
 
